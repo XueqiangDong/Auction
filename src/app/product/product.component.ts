5a4cd7f9fd99c4f9bf9ac29f85ab2150c9513d4e
Xueqiang Dong <dxq7541@gmail.com>
2018-04-13 15:28:37 -0400

    Component first version finish all

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  private product: Array<Product>;
+  public imgUrl = 'http://placehold.it/320x150';
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.product = [
+      new Product(1, '第1个商品', 1.99, 0, '吸尘器，清洗灰尘', ['电子产品', '装修家具']),
+      new Product(2, '第2个商品', 2.99, 4.5, '吸尘器，清洗灰尘', ['电子产品', '装修家具']),
+      new Product(3, '第3个商品', 10.99, 3.5, '吸尘器，清洗灰尘', ['电子产品', '装修家具', '灌溉系统']),
+      new Product(4, '第4个商品', 16.99, 1.5, '吸尘器，清洗灰尘', ['电子产品', '装修家具']),
+      new Product(5, '第5个商品', 41.99, 4.5, '吸尘器，清洗灰尘', ['电子产品', '装修家具']),
+      new Product(6, '第6个商品', 441.99, 5, '吸尘器，清洗灰尘', ['电子产品', '装修家具'])
+    ];
+  }
+
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {
+
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ImportMetadata} from '@angular/compiler-cli/src/metadata/evaluator';
 
 @Component({
   selector: 'app-stars',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
-  constructor() { }
+  protected stars: number[];
+  @Input()
+  protected rating: number = 0;
+
+  constructor() {
+  }
 
   ngOnInit() {
+
+    this.stars = [];
+    switch (this.rating) {
+      case 0:
+        this.stars = [0, 0, 0, 0, 0];
+        break;
+      case 0.5:
+        this.stars = [1, 0, 0, 0, 0];
+        break;
+      case 1:
+        this.stars = [2, 0, 0, 0, 0];
+        break;
+      case 1.5:
+        this.stars = [2, 1, 0, 0, 0];
+        break;
+      case 2:
+        this.stars = [2, 2, 0, 0, 0];
+        break;
+      case 2.5:
+        this.stars = [2, 2, 1, 0, 0];
+        break;
+      case 3:
+        this.stars = [2, 2, 2, 0, 0];
+        break;
+      case 3.5:
+        this.stars = [2, 2, 2, 1, 0];
+        break;
+      case 4:
+        this.stars = [2, 2, 2, 2, 0];
+        break;
+      case 4.5:
+        this.stars = [2, 2, 2, 2, 1];
+        break;
+      case 5:
+        this.stars = [2, 2, 2, 2, 2];
+        break;
+      default: this.stars = [0, 0, 0, 0, 0];
+    }
   }
 
 }
